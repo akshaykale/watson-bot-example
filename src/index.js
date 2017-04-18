775a7912e9a434ede108676cc160b690b6e62b62
akshaykale <ark.akshaykale@gmail.com>
2017-04-18 13:36:42 +0900

    rich media

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 const ViberBot = require("viber-bot").Bot;
 const BotEvents = require("viber-bot").Events;
 const TextMessage = require("viber-bot").Message.Text;
+const RichMedia = require("viber-bot").Message.RichMedia;
 
 var RestClient = require('node-rest-client').Client;
 var restClient = new RestClient();
